572e58601f1e48cff45d417881f743ebce21316b
Joe Eames <joeeames@gmail.com>
2013-03-02 01:10:29 -0700

    new event and session, and some styling

--- app/js/app.js
+++ app/js/app.js
@@ -4,7 +4,9 @@
 angular.module('eventsApp', ['eventsApp.filters', 'eventsApp.services', 'eventsApp.directives']).
   config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $routeProvider.when('/events', {templateUrl: '/partials/eventList.html', controller: EventListController});
+    $routeProvider.when('/events/:eventId/sessions/new', {templateUrl: '/partials/newSession.html', controller: NewSessionController});
     $routeProvider.when('/event/:eventId', {templateUrl: '/partials/event.html', controller: EventController});
+    $routeProvider.when('/events/new', {templateUrl: '/partials/newEvent.html', controller: NewEventController});
     $routeProvider.otherwise({redirectTo: '/view1'});
     $locationProvider.html5Mode(true);
   }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -10,3 +10,25 @@ function EventController($scope, $routeParams, eventData) {
     $scope.event = _.findWhere(eventData.events, {id: parseInt($routeParams.eventId)});
 }
 EventController.$inject = ['$scope', '$routeParams', 'eventData'];
+
+
+function NewEventController($scope, eventData) {
+    $scope.event = {};
+
+    $scope.saveEvent = function(event) {
+        eventData.events.push(event);
+    }
+}
+NewEventController.$inject = ['$scope', 'eventData'];
+
+
+function NewSessionController($scope, eventData, $routeParams) {
+    $scope.event = _.findWhere(eventData.events, {id: parseInt($routeParams.eventId)});
+
+    $scope.session = {}
+
+    $scope.saveSession = function(session) {
+        $scope.event.sessions.push(session);
+    }
+}
+NewSessionController.$inject = ['$scope', 'eventData', '$routeParams'];
