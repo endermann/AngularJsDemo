65d3cbf9e53cdd895ae72dbb19836b03c21237f3
joeeames <joeeames@gmail.com>
2013-04-01 23:43:05 -0600

    final touches on the branch

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -4,21 +4,38 @@ eventsApp.controller('EventController',
     function EventController($scope) {
     	$scope.sortorder = 'name';
         $scope.event = {
-            name: 'my event',
+            name: 'Angular Boot Camp',
             date: '1/1/2013',
             time: '10:30 am',
             location: {
-                address: '123 fake st',
-                city: 'SLC',
-                province: 'UT'
+                address: 'Google Headquarters',
+                city: 'Mountain View',
+                province: 'CA'
             },
-            imageUrl: 'http://blog.laptopmag.com/wpress/wp-content/uploads/2012/08/Code-Camp_sf.jpg',
+            imageUrl: 'http://pascalprecht.github.com/slides/angularjs-insights/img/angularjs-logo.png',
             sessions: [
                 {
-                    name: 'Learn to Program',
-                    creatorName: 'Bob Martin',
-                    duration: '1hr',
-                    abstract: 'In this session you will learn to Program like a champ!',
+                    name: 'Directives Masterclass',
+                    creatorName: 'Bob Smith',
+                    duration: 1,
+                    level: 'Advanced',
+                    abstract: 'In this session you will learn the ins and outs of directives!',
+                    upVoteCount: 0
+                },
+                {
+                    name: 'Scopes for fun and profit',
+                    creatorName: 'John Doe',
+                    duration: 3,
+                    level: 'Beginner',
+                    abstract: 'This session will take a closer look at scopes.  Learn what they do, how they do it, and how to get them to do it for you.',
+                    upVoteCount: 0
+                },
+                {
+                    name: 'Well Behaved Controllers',
+                    creatorName: 'Jane Doe',
+                    duration: 2,
+                    level: 'Intermediate',
+                    abstract: 'Controllers are the beginning of everything Angular does.  Learn how to craft controllers that will win the respect of your friends and neighbors.',
                     upVoteCount: 0
                 }
             ]
