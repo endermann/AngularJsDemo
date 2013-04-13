5c5f0593433481c4ca8b1c1ddd99103d0cf8acea
Jim Cooper <jim@cooperplanet.com>
2013-04-13 11:01:38 -0600

    After module3 clip 5

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -3,7 +3,13 @@
 eventsApp.controller('EventController',
     function EventController($scope, eventData) {
     	$scope.sortorder = 'name';
-        $scope.event = eventData.event;
+        $scope.event = eventData.getEvent();
+        $scope.event.then(function(event) {
+            console.log(event);
+        }, function(status) {
+            console.log(status);
+        });
+
 
         $scope.upVoteSession = function(session) {
             session.upVoteCount++;
