c473e2b3c6c7955cb10bf48f91e37dae6a848769
Jim Cooper <jim@cooperplanet.com>
2013-04-16 20:57:48 -0600

    Added exceptionhandler

--- app/js/controllers/LocaleSampleController.js
+++ app/js/controllers/LocaleSampleController.js
@@ -2,10 +2,11 @@
 
 eventsApp.controller('LocaleSampleController',
     function LocaleSampleController($scope, $locale) {
-
         console.log($locale);
         $scope.myDate = Date.now();
         $scope.myFormat = $locale.DATETIME_FORMATS.fullDate;
+
+        //throw { message: 'error message' };
 
     }
 );
