e6cbae53048471b07e787c1c658e94e5f30888b9
joeeames <joeeames@gmail.com>
2013-05-04 11:28:33 -0600

    changed some variable names in the event thumbnail tests

--- test/unit/eventThumbnailSpec.js
+++ test/unit/eventThumbnailSpec.js
@@ -1,7 +1,7 @@
 'use strict';
 
 describe('eventThumbnail', function() {
-    var el, stubNavigateToDetails;
+    var el, stubShowDetails;
 
     beforeEach(module('eventsApp'));
     beforeEach(module('partials/directives/eventThumbnail.html'));
@@ -9,25 +9,24 @@ describe('eventThumbnail', function() {
     beforeEach(inject(function($compile, $rootScope) {
         // set up scope
         var scope = $rootScope;
-        scope.event = {name: 'Event Name', date: '223', time: '334', location: {address: '1231', city: '1414', province: '1515'}};
-        stubNavigateToDetails = sinon.stub();
-        scope.navigateToDetails = stubNavigateToDetails;
+        scope.event = {name: 'Event Name', date: '223', time: '334', location: {address: '1234', city: '1414', province: '1515'}};
+        stubShowDetails = sinon.stub();
+        scope.myShowDetails = stubShowDetails;
 
         // create and compile directive
-        el = angular.element('<event-thumbnail event="event" show-details="navigateToDetails(event)"/>');
+        el = angular.element('<event-thumbnail event="event" show-details="myShowDetails(event)" />');
         $compile(el)(scope);
         scope.$digest();
+        console.log(el[0].outerHTML);
     }));
+
+    it('should bind the data', function() {
+        expect(el.text()).toContain('Event Name');
+    });
 
     it('should bind to the scope\'s event', function() {
         el.click();
 
-        expect(stubNavigateToDetails.called).toBe(true);
-    });
-
-    it('should bind the data', function() {
-        expect(el.text()).toContain('Event Name');
-        expect(el.text()).not.toContain('{{');
-        expect(el.find('.row').eq(3).text()).toContain('Location: 1231');
+        expect(stubShowDetails.called).toBe(true);
     });
 });
